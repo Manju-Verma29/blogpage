94af69cfc0b03f4826dd1c2c0ac5f1502afb9c69
Manju Verma <manjuverma2906@gmail.com>
2026-02-28 21:05:05 +0530

    feat: add more links and update logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,12 +5,14 @@ import logo from '../assets/logo.png'
 export default function Navbar() {
   return (
     <>
-    <div className=' flex p-3 text-neutral-400 justify-between font-semi-bold'>
+    <div className='flex px-3 bg-white border-b border-blue-900 sticky top-0 z-50 w-full  text-amber-950 font-semibold justify-between font-semi-bold'>
     <img src={logo} alt="" className='w-35 lg:w-50' />
-    <nav className='flex justify-center gap-3 items-center'>
+    <nav className='flex justify-center  gap-3 items-center'>
         <Link to='/'>Home</Link>
+        <Link to='/blog'>Blog</Link>
         <Link to="/checklist">Checklist</Link>
         <Link to="/progress">Progress</Link>
+        <Link to="/books">Books</Link>
     </nav>
     </div>
     </>

0959b699645acaacba1986fbe684574ac96836b4
Manju Verma <manjuverma2906@gmail.com>
2026-02-28 21:12:36 +0530

    feat: add Navbar, footer, homepage routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Blogpage from "./Components/Blogpage"
+import Footer from "./Components/Footer"
+import Homepage from "./Components/Homepage"
+import Navbar from "./Components/Navbar"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Blogpage/>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/blog" element={<Blogpage/>}/>
+    </Routes>
+    <Footer/>
     </>
   )
 }

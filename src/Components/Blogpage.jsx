e3d944485fa19881085b0ba5236cd45407929c6f
Manju Verma <manjuverma2906@gmail.com>
2026-02-28 21:09:25 +0530

    style: integrate tailwind

--- src/Components/Blogpage.jsx
+++ src/Components/Blogpage.jsx
@@ -2,22 +2,23 @@ import {lifeGoals} from "./data.js"
 
 export default function Blogpage() {
   return (
-    <>
+    <>    
     {lifeGoals.map((category, index)=> (
-        <div key={index} className="p-8  lg:max-w-5xl lg:mx-auto">
-        <h2 className="text-3xl font-bold">{category.title}</h2>
+        <div key={index} className="mx-8 text-amber-950 my-3.5 lg:max-w-5xl lg:mx-auto">
+        <h1 className="text-4xl py-10">What to Do in Your 20s: A Complete Guide</h1>
+        <h2 className="text-xl lg:text-3xl  font-bold">{category.title}</h2>
         <ul>
             {category.subcategories.map((sub, subIndex)=>(
             <li key={subIndex} className="px-8 py-2">
-                <h3 className="text-2xl py-3 font-bold">{sub.subTitle}</h3>
-                <div className="px-10">
-                <p className="text-lg">{sub.contentTitle1 && <strong>{sub.contentTitle1}</strong>}
+                <h3 className="text-md lg:text-2xl lg:py-3 font-bold">{sub.subTitle}</h3>
+                <div className="lg:px-10 px-2 grid gap-2">
+                <p className="text-base lg:text-lg ">{sub.contentTitle1 && <strong>{sub.contentTitle1}</strong>}
                 {sub.content1}</p>
                 
-                <p className="text-lg">{sub.contentTitle2 && <strong>{sub.contentTitle2}</strong>}
+                <p className="text-base lg:text-lg">{sub.contentTitle2 && <strong>{sub.contentTitle2}</strong>}
                 {sub.content2}</p>
 
-                <p className="text-lg">{sub.contentTitle3 && <strong>{sub.contentTitle3}</strong>}
+                <p className="text-base lg:text-lg">{sub.contentTitle3 && <strong>{sub.contentTitle3}</strong>}
                 {sub.content3}</p>
                 </div>
             </li>

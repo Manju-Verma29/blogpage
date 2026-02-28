dc4ae996fc773e2bec4972cfda55ef0e84a1e800
Manju Verma <manjuverma2906@gmail.com>
2026-02-28 21:13:22 +0530

    feat: add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <>
+    <article  className='my-4'>
+        <p className='text-lg italic text-center'>Your future self is watching what you do today.</p>
+    </article>
+    </>
+  )
+}

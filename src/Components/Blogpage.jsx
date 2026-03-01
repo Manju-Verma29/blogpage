4ad19cd250e6d8630ad2be353bcf24c0aa24566c
Manju Verma <manjuverma2906@gmail.com>
2026-03-01 20:34:04 +0530

    style: update text color and layout adjustments

--- src/Components/Blogpage.jsx
+++ src/Components/Blogpage.jsx
@@ -2,15 +2,17 @@ import {lifeGoals} from "./data.js"
 
 export default function Blogpage() {
   return (
-    <>    
+    <> 
+    <div>
+      <h1 className="text-5xl mx-8 my-3.5 lg:max-w-5xl lg:mx-auto py-10 text-center text-yellow-400">What to Do in Your 20s: A Complete Guide</h1>   
+    </div>
     {lifeGoals.map((category, index)=> (
-        <div key={index} className="mx-8 text-amber-950 my-3.5 lg:max-w-5xl lg:mx-auto">
-        <h1 className="text-4xl py-10">What to Do in Your 20s: A Complete Guide</h1>
-        <h2 className="text-xl lg:text-3xl  font-bold">{category.title}</h2>
+        <div key={index} className="mx-8  my-12 lg:max-w-5xl lg:mx-auto">
+        <h2 className="text-xl lg:text-3xl text-blue-900 font-bold">{category.title}</h2>
         <ul>
             {category.subcategories.map((sub, subIndex)=>(
-            <li key={subIndex} className="px-8 py-2">
-                <h3 className="text-md lg:text-2xl lg:py-3 font-bold">{sub.subTitle}</h3>
+            <div key={subIndex} className="px-8 text-blue-900 py-2">
+                <h3 className="text-md lg:text-2xl lg:py-3 font-bold ">{sub.subTitle}</h3>
                 <div className="lg:px-10 px-2 grid gap-2">
                 <p className="text-base lg:text-lg ">{sub.contentTitle1 && <strong>{sub.contentTitle1}</strong>}
                 {sub.content1}</p>
@@ -21,7 +23,7 @@ export default function Blogpage() {
                 <p className="text-base lg:text-lg">{sub.contentTitle3 && <strong>{sub.contentTitle3}</strong>}
                 {sub.content3}</p>
                 </div>
-            </li>
+            </div>
             ))}
         </ul>
         </div>

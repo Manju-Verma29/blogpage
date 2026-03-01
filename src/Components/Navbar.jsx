0424719a9afb67fc22cbce1a125d3c38712639b1
Manju Verma <manjuverma2906@gmail.com>
2026-03-01 19:11:00 +0530

    refactor: replace Link to NavLink for navigation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,20 @@
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import logo from '../assets/logo.png'
 
 
 export default function Navbar() {
+  const linkClass = ({isActive}) => isActive ? "text-blue-900 font-bold border-b-2" : "text-blue-900 hover:underline";
+
   return (
     <>
-    <div className='flex px-3 bg-white border-b border-blue-900 sticky top-0 z-50 w-full  text-amber-950 font-semibold justify-between font-semi-bold'>
+    <div className='flex px-3 bg-blue-50  border-blue-900 sticky top-0 z-50 w-full  text-amber-950 font-semibold justify-between font-semi-bold'>
     <img src={logo} alt="" className='w-35 lg:w-50' />
-    <nav className='flex justify-center  gap-3 items-center'>
-        <Link to='/'>Home</Link>
-        <Link to='/blog'>Blog</Link>
-        <Link to="/checklist">Checklist</Link>
-        <Link to="/progress">Progress</Link>
-        <Link to="/books">Books</Link>
+    <nav className='flex justify-center text-blue-900 gap-3 items-center'>
+        <NavLink to="/" className={linkClass}>Home</NavLink>
+        <NavLink to="/blog" className={linkClass}>Blog</NavLink>
+        <NavLink to="/checklist" className={linkClass}>Checklist</NavLink>
+        <NavLink to="/progress" className={linkClass}>Progress</NavLink>
+        <NavLink to="/books" className={linkClass}>Books</NavLink>
     </nav>
     </div>
     </>

0dd66d886f9ac7b98219375e81b3b9b37d8308e3
Manju Verma <manjuverma2906@gmail.com>
2026-03-02 20:18:02 +0530

    style: improve the layout

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,96 +1,91 @@
-import { focus } from "./data"
-import  home  from "../assets/home.png"
+import { focus, homeMain, thewhy, commonMistakes } from "./data"
+import home from "../assets/home.png"
 
 export default function Homepage() {
-  return (
-    <>    
-    {/* <section className="h-screen p-40 grid grid-cols-[2fr-1fr] place-items-start"> */}
-    <section className="min-h-screen md:p-10 lg:p-40 grid grid-cols-[2fr_1fr] items-center">
-        <div className="grid gap-20">
-        <header>
-            <h1 className="sm:text-xl md:text-3xl lg:text-6xl text-blue-900 md:leading-16 lg:leading-28 font-serif">Your 20s Are Not for Surviving,</h1>
-            <h1 className="sm:text-md md:text-xl lg:text-5xl text-yellow-400 font-mono">They’re for Building.</h1>
-        </header>
+    return (
+        <>
+            {/* main */}
+            <section className="min-h-screen px-6 py-12 md:px-10 lg:px-40 lg:py-20 grid lg:grid-cols-[2fr_1fr] items-center gap-12">
+                <div className="grid gap-10 text-center lg:text-left">
+                    <header>
+                        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl text-blue-900 leading-tight font-serif">{homeMain.title1}</h1>
+                        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-5xl text-yellow-400 font-mono">{homeMain.title2}</h1>
+                    </header>
 
-        <div className="grid sm:gap-1 md:gap-3 lg:gap-6">
-            <h3  className="text-blue-500 md:text-lg lg:text-2xl">A practical roadmap for your 20s — career, money, health, mindset, relationships, and growth.</h3>
-        <div className="flex gap-5">
-            <button className="border rounded p-3 font-sans font-semibold hover:bg-blue-900 hover:shadow-2xl hover:shadow-blue-900 hover:text-white text-blue-900 transform transition duration-300 cursor-pointer">Start Your Journey</button>
-            <button className="border rounded p-3 font-sans font-semibold hover:bg-blue-900 hover:shadow-2xl hover:shadow-blue-900 hover:text-white text-blue-900 transform transition duration-200 cursor-pointer">Explore the Guide</button>
-        </div>
-        </div>
-        </div>
-            <div className=" flex justify-center lg:justify-end ">
-                <img src={home} alt="Building your 20s" className="w-80 lg:w-96 object-contain"/>
-            </div>
-    </section>
-
-
-    <section className="min-h-screen px-40 ">
-        <header className="grid gap-5">
-            <h1 className="text-6xl text-yellow-400">Why Your 20s Matter?</h1>
-            <h3 className="text-3xl text-blue-900">Your 20s shape the next 40 years of your life.</h3>
-        </header>
-
-        <div className="m-12 text-blue-900">
-        <p className="text-2xl ">This is the decade where:</p>
-        <ul className="list-disc  list-outside pl-5 text-xl p-5 ">
-            <li className="p-3">You build your career foundation</li>
-            <li className="p-3">You shape your habits</li>
-            <li className="p-3">You define your standards</li>
-            <li className="p-3">You learn who you really are</li>
-        </ul>
-        </div>
-
-        <div className="text-blue-900 grid text-xl gap-4">
-            <p>Most people waste their 20s chasing trends. Smart people invest in skills, health, and clarity.</p>
-        </div>
-    </section>
-
-    <section className="ticker-wrap text-center bg-blue-900 w-full space-y-8 py-8">
-        <header>
-            <h1 className="text-yellow-400 text-2xl font-serif ">Common Mistakes in Your 20s</h1>
-        </header>
-        <div className=" text-xl p-4 text-blue-50 ticker-wrapper">
-            <div className="ticker-track gap-16">
-                <p>Waiting for motivation</p>
-                <p>Comparing yourself to social media</p>
-                <p>Ignoring health</p>
-                <p>Not learning about money</p>
-                <p>Staying in comfort zones</p>
-            
-                <p>Waiting for motivation</p>
-                <p>Comparing yourself to social media</p>
-                <p>Ignoring health</p>
-                <p>Not learning about money</p>
-                <p>Staying in comfort zones</p>
-            </div>  
-        </div>
-        <p className="text-blue-50 italic">
-            You don’t need to be perfect. You need direction.
-        </p>
-    </section>
-
-    <section className="min-h-screen p-40">
-        <header>
-            <h1 className="text-6xl text-yellow-400">What You Should Focus On?</h1>
-        </header>
-
-        <div className="flex gap-5 py-14">
-            {focus.map((item, index) => (
-                <div key={index} className="border-2 border-blue-200 w-md p-8 rounded-md hover:shadow-lg  flex flex-col justify-between">
-                    <div>
-                    <p className="font-bold text-blue-900">{item.title}</p>
-                    <p >{item.content.line1}</p>
-                    <p >{item.content.line2}</p>
-                    <p >{item.content.line3}</p>
-                    <p >{item.content.line4}</p>
+                    <div className="grid sm:gap-1 md:gap-3 lg:gap-6">
+                        <h3 className="text-blue-500 md:text-lg lg:text-2xl">{homeMain.mainSub}</h3>
+                        <div className="flex flex-col sm:flex-row gap-4 p-4 justify-center lg:justify-start">
+                            {homeMain.buttons.map((btn, i) => (
+                                <button key={i} className="border rounded p-3 font-sans font-semibold hover:bg-blue-900 hover:shadow-2xl hover:shadow-blue-900 hover:text-white text-blue-900 transform transition duration-300 cursor-pointer">{btn}</button>
+                            ))}
+                        </div>
                     </div>
-                    <p className="italic text-left border-t text-gray-700 border-gray-200 pt-2">{item.advice}</p>
                 </div>
-            ))}
-        </div>
-    </section> 
-    </>
-  )
+                {home && (
+                    <div className="hidden lg:block justify-center lg:justify-end ">
+                        <img src={home} alt="Building your 20s" className="w-80 lg:w-96 object-contain" />
+                    </div>
+                )}
+            </section>
+
+
+            {/* Why */}
+            <section className="lg:min-h-screen px-4 py-14 sm:px-6 md:px-10 lg:px-32">
+                <header className="space-y-6 text-center lg:text-left">
+                    <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-yellow-400">{thewhy.title}</h1>
+                    <h3 className="text-lg md:text-xl lg:text-2xl text-blue-900">{thewhy.subTitle}</h3>
+                </header>
+                <div className="m-8 text-blue-900">
+                    <p className="text-base md:text-lg">{thewhy.intro}</p>
+                    <ul className="list-disc list-outside pl-1 lg:pl-5  lg:p-5 ext-sm sm:text-base md:text-lg ">
+                        {thewhy.points.map((lines, index) => (
+                            <li className="p-3" key={index}>{lines}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="text-blue-900 text-sm md:text-lg">
+                    <p>{thewhy.last}</p>
+                </div>
+            </section>
+
+            {/* mistakes */}
+            <section className="ticker-wrap text-center bg-blue-900 w-full space-y-8 py-4 md:py-6 lg:py-8">
+                <header>
+                    <h1 className="text-yellow-400 sm:text-md md:text-lg lg:text-3xl font-serif ">{commonMistakes.title}</h1>
+                </header>
+
+                <div className="text-base lg:text-xl lg:p-4 text-blue-50 ticker-wrapper overflow-hidden">
+                    <div className="flex ticker-track gap-16">
+                        {[...commonMistakes.points, ...commonMistakes.points].map((points, index) => (
+                            <p key={index}>{points}</p>
+                        ))}
+                    </div>
+                </div>
+
+                <p className="text-blue-50 sm:text-xs lg:text-lg italic">{commonMistakes.last}</p>
+            </section>
+
+            {/* focus */}
+            <section className="min-h-screen p-4 sm:px-8 lg:px-20 py-16 overflow-x-hidden">
+                <header className="text-center mb-12">
+                    <h1 className="text-2xl sm:text-4xl lg:text-6xl text-yellow-400">What You Should Focus On?</h1>
+                </header>
+
+                <div className="flex flex-wrap gap-6 justify-center">
+                    {focus.map((item, index) => (
+                        <div key={index} className="border-2 border-blue-200 w-md p-8 rounded-md hover:shadow-lg  flex flex-col justify-between">
+                            <div>
+                                <p className="font-bold text-blue-900">{item.title}</p>
+                                <p >{item.content.line1}</p>
+                                <p >{item.content.line2}</p>
+                                <p >{item.content.line3}</p>
+                                <p >{item.content.line4}</p>
+                            </div>
+                            <p className="italic text-left border-t text-gray-700 border-gray-200 pt-2">{item.advice}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    )
 }

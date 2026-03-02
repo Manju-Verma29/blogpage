973af65473eefcd21d5784eadf8c30f8c392c715
Manju Verma <manjuverma2906@gmail.com>
2026-03-02 20:11:49 +0530

    refactor: improve layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,13 @@ import logo from '../assets/logo.png'
 
 
 export default function Navbar() {
-  const linkClass = ({isActive}) => isActive ? "text-blue-900 font-bold border-b-2" : "text-blue-900 hover:underline";
+  const linkClass = ({isActive}) => isActive ? "text-blue-900 font-bold border-b-2 text-xs md:text-md lg:text-lg" : "text-xs md:text-md lg:text-lg text-blue-900 hover:underline";
 
   return (
     <>
-    <div className='flex px-3 bg-blue-50  border-blue-900 sticky top-0 z-50 w-full  text-amber-950 font-semibold justify-between font-semi-bold'>
-    <img src={logo} alt="" className='w-35 lg:w-50' />
-    <nav className='flex justify-center text-blue-900 gap-3 items-center'>
+    <div className='flex px-3 lg:px-8 bg-blue-50 sticky top-0 z-50 w-full max-w-full overflow-hidden text-amber-950 font-semibold justify-between font-semi-bold'>
+    <img src={logo} alt="lofo" className=' md:w-35 w-20 lg:w-50' />
+    <nav className='flex justify-center text-blue-900 gap-1.5 lg:gap-3 items-center'>
         <NavLink to="/" className={linkClass}>Home</NavLink>
         <NavLink to="/blog" className={linkClass}>Blog</NavLink>
         <NavLink to="/checklist" className={linkClass}>Checklist</NavLink>
